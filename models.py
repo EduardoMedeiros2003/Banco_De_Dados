edac9a0d8f3f26a482fee008608fb024636b9d5b
Eduardo Umbelino <umbelino.eduardo2003@gmail.com>
2026-02-02 21:15:55 -0300

    add schemas
    Criação da estrutura de cada elemento do banco de dados

--- models.py
+++ models.py
@@ -5,10 +5,10 @@ class Estudantes(Base):
     __tablename__ = 'estudante'
     id = Column(Integer, primary_key=True, index=True)# id_estudante, coluna - inteiro - chave primaria - vai cirar altomaticamente(index)
     nome = Column(String(100), nullable=False)#nome, coluna - string - nao pode esta vazio
-    age = Column(Integer)
+    idade = Column(Integer)
 
 class Matricula(Base):
     __tablename__ = 'matricula'
     id = Column(Integer, primary_key=True, index=True)#id_matricula, coluna , chave primaria- vai criar altomaticamente
-    student_id = Column(Integer, ForeignKey('estudante.id'))#id_estudante, coluna - inteiro - chave estrangeira
+    estudante_id = Column(Integer, ForeignKey('estudante.id'))#id_estudante, coluna - inteiro - chave estrangeira
     nome_disciplina = Column(String(100), nullable=False)
